57ada677026453bb2f0b2bb4173deff99e9d3a76
JakubKub13 <jakubkubala3@gmail.com>
2023-01-03 13:28:01 +0100

    Adding wrap func

--- Text_summarization.py
+++ Text_summarization.py
@@ -11,3 +11,9 @@ data_frame = pd.read_csv('bbc_text_cls.csv')
 data_frame.head()
 
 doc = data_frame[data_frame.labels == 'business']['text'].sample(random_state=42)
+
+def wrap(x):
+  return textwrap.fill(x, replace_whitespace=False, fix_sentence_endings=True)
+
+print(wrap(doc.iloc[0]))
+
