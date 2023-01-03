b015d4d2fab1da7f4a0035870d7d5de98778d0f3
JakubKub13 <jakubkubala3@gmail.com>
2023-01-03 13:27:17 +0100

    checking data frame

---
+++ Text_summarization.py
@@ -0,0 +1,13 @@
+# https://www.kaggle.com/shivamkushwaha/bbc-full-text-document-classification
+#!wget -nc https://lazyprogrammer.me/course_files/nlp/bbc_text_cls.csv
+#pip install transformers
+
+import pandas as pd
+import numpy as np
+import textwrap
+from transformers import pipeline
+
+data_frame = pd.read_csv('bbc_text_cls.csv')
+data_frame.head()
+
+doc = data_frame[data_frame.labels == 'business']['text'].sample(random_state=42)
